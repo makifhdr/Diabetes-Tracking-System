6b0133def61f8af52177fbaa7b753bd2fe903c40
Mehmet Akif <makifhidir92@gmail.com>
2025-09-21 13:50:17 +0300

    DB url change

--- src/application/VTBaglantisi.java
+++ src/application/VTBaglantisi.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class VTBaglantisi {
-	private static final String URL = "jdbc:postgresql://localhost:5432/diyabet_takip";
+	private static final String URL = "jdbc:postgresql://localhost:5432/diyabet_takip?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 	private static final String USER = "postgres";
 	private static final String PASSWORD = "Makifhidir61*";
 	
